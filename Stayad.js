f07a58e5f1e669f875db850c548b6f5bab176cff
动物园 <110404406+Crazy-Z7@users.noreply.github.com>
2022-11-30 16:45:33 +0800

    Update Stayad.js

--- Stayad.js
+++ Stayad.js
@@ -2,4 +2,6 @@
 var body=$response.body;
 body = body.replace(/imageUrl\":".*?"/g,'imageUrl":"null"');
 body = body.replace(/jumpUrl\":".*?"/g,'jumpUrl":"null"');
+body = body.replace(/subtitle_cn\":".*?"/g,'subtitle_cn":""');
+body = body.replace(/title_cn\":".*?"/g,'title_cn":""');
 $done(body);
