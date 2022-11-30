d51bd8ca9e46b9e8e1e413e2cd60e9e79a4d71f5
动物园 <110404406+Crazy-Z7@users.noreply.github.com>
2022-11-30 21:04:58 +0800

    Create 12306.js

---
+++ 12306.js
@@ -0,0 +1,8 @@
+
+
+var body=$response.body;
+body = body.replace(/skipTime\":\d/g,'skipTime":-1');
+body = body.replace(/skipTimeAgain\":\d/g,'skipTimeAgain":-1');
+body = body.replace(/showSkipBtn\":\d/g,'showSkipBtn":-1');
+body = body.replace(/filePath\":".*?"/g,'filePath":"null"');
+$done(body);
