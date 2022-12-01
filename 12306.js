dbd364c950dc241270635eb319e6b30d3053bee4
动物园 <110404406+Crazy-Z7@users.noreply.github.com>
2022-12-01 09:00:01 +0800

    Update 12306.js

--- 12306.js
+++ 12306.js
@@ -1,7 +1,7 @@
 
 
 var body=$response.body;
-body = body.replace(/skipTime\":\d/g,'skipTime":-1');
+body = body.replace(/skipTime\":\d+/g,'skipTime":-1');
 body = body.replace(/skipTimeAgain\":\d/g,'skipTimeAgain":-1');
 body = body.replace(/showSkipBtn\":\d/g,'showSkipBtn":-1');
 body = body.replace(/filePath\":".*?"/g,'filePath":"null"');
