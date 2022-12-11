1fba14e25acc3c60eaadbe3f184ceab2802bd4ff
动物园 <110404406+Crazy-Z7@users.noreply.github.com>
2022-12-11 15:16:45 +0800

    Update 12306.js

--- 12306.js
+++ 12306.js
@@ -1,8 +1,16 @@
-
-
 var body=$response.body;
-body = body.replace(/skipTime\":\d+/g,'skipTime":-1');
-body = body.replace(/skipTimeAgain\":\d/g,'skipTimeAgain":-1');
-body = body.replace(/showSkipBtn\":\d/g,'showSkipBtn":-1');
-body = body.replace(/filePath\":".*?"/g,'filePath":"null"');
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const Zoo = '/ad/ser/getAdList'
+
+if (url.indexOf(Zoo) != -1) {
+     obj.materialsList.filePath ="null";
+     obj.advertParam.skipTime=1;
+     obj.advertParam.skipTimeAgain=-1;
+     obj.advertParam.showSkipBtn=-1;
+ 
+body = JSON.stringify(obj);
+}
+
 $done(body);
