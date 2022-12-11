dc05cfc9ed3c1f7fee82dae8074019a663c745d4
动物园 <110404406+Crazy-Z7@users.noreply.github.com>
2022-12-11 15:33:06 +0800

    Create Stayad.js

--- Stayad.js
+++ Stayad.js
@@ -7,10 +7,10 @@ var obj = JSON.parse(body);
 const Zoo = '/stay-fork/browse/featured'
 
 if (url.indexOf(Zoo) != -1) {
-     obj.blocks.imageUrl ="null";
-    obj.blocks.jumpUrl ="null";
-    obj.blocks.subtitle_cn ="null";
-      obj.blocks.title_cn ="null";
+     obj.biz.imageUrl ="null";
+    obj.biz.jumpUrl ="null";
+    obj.biz.subtitle_cn ="null";
+      obj.biz.title_cn ="null";
  
 body = JSON.stringify(obj);
 }
