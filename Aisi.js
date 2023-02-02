96c2a991d3dfd2402776c21223156b5252649589
动物园 <110404406+Crazy-Z7@users.noreply.github.com>
2023-02-02 21:33:40 +0800

    Update Aisi.js

--- Aisi.js
+++ Aisi.js
@@ -1,5 +1,16 @@
 
-var body=$response.body;
-body = body.replace(/content\":".*?"/g,'content":"null"');
-body = body.replace(/image\":".*?"/g,'image":"null"');
-$done(body);
+
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
+
+let item = [
+  "adli", //搜索页推荐广告
+  "ad" , //精选页顶部广告  
+];
+if (obj) {
+  item.forEach((i) => {
+    delete obj[i];
+  });
+}
+
+$done({ body: JSON.stringify(obj) });
