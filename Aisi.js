c30756009d17f3797ff95b4b6a0e2ece06ae6929
动物园 <110404406+Crazy-Z7@users.noreply.github.com>
2023-02-02 22:27:02 +0800

    Update Aisi.js

--- Aisi.js
+++ Aisi.js
@@ -1,11 +1,11 @@
-
-
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
 let item = [
   "adli", //搜索页推荐广告
-  "ad" , //精选页顶部广告  
+  "ad", //精选页顶部广告
+  "spappli", //大家都爱用 
+  "tmpOrderIndex", //应用顶部广告
 ];
 if (obj) {
   item.forEach((i) => {
