1d08cb49770b07e08745727cace93bb0c5a7f301
动物园 <110404406+Crazy-Z7@users.noreply.github.com>
2023-04-11 19:38:49 +0800

    Update zhy.js

--- zhy.js
+++ zhy.js
@@ -1,9 +1,9 @@
 var obj = JSON.parse($response.body);
-obj.data.url = null;
-obj.data.city_advert_startup_times = 1;
-obj.data.new_thumb = null;
-obj.data.thumb = null;
-obj.data.title = "去除开屏";
-obj.data.thumb = null;
+obj.data.openScreenAdvertise.url = null;
+obj.data.openScreenAdvertise.city_advert_startup_times = 1;
+obj.data.openScreenAdvertise.new_thumb = null;
+obj.data.openScreenAdvertise.thumb = null;
+obj.data.openScreenAdvertise.title = "去除开屏";
+obj.data.openScreenAdvertise.thumb = null;
 
 $done({body : JSON.stringify(obj)});
