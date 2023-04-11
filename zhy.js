ce97d3fba38032c5b2e10d515de3c55fea6f837b
动物园 <110404406+Crazy-Z7@users.noreply.github.com>
2023-04-11 19:52:21 +0800

    Delete zhy.js

--- zhy.js
+++
@@ -1,9 +0,0 @@
-var obj = JSON.parse($response.body);
-obj.data.openScreenAdvertise.url = null;
-obj.data.openScreenAdvertise.city_advert_startup_times = 1;
-obj.data.openScreenAdvertise.new_thumb = null;
-obj.data.openScreenAdvertise.thumb = null;
-obj.data.openScreenAdvertise.title = "去除开屏";
-obj.data.openScreenAdvertise.thumb = null;
-
-$done({body : JSON.stringify(obj)});
