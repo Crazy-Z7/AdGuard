1aec6a95209f06996625c4698982666e8a17eef7
Zoo <110404406+Crazy-Z7@users.noreply.github.com>
2023-05-30 00:19:52 +0800

    Update Aisi.js

--- Aisi.js
+++ Aisi.js
@@ -1,5 +1,16 @@
-
-
+/**********************************************
+作者:Zoo
+#爱思助手屏蔽开屏广告
+#去除发现页广告
+#去除精选顶部广告
+日期:2023.05.30
+[rewrite_local]
+^https?:\/\/.*\.i4\.cn\/getopfstadinfo\.* url reject-200
+^https?:\/\/list-app-m\.i4\.cn\/getHotSearchList\.xhtml url reject-200
+^http:\/\/d\.image\.i4\.cn\/adInfo\/image\/original\/*\.jpg url reject
+^https?:\/\/list-app-m\.i4\.cn\/(getAppList|getHotSearchList)\.xhtml url script-response-body https://raw.githubusercontent.com/Crazy-Z7/AdGuard/main/Aisi.js
+hostname = *.i4.*
+**********************************************/
 let obj = JSON.parse($response.body);
 
 let item = [
