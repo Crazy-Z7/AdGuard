649b5b0e66991b413c0ff391efdf40570a987c77
Zoo <110404406+Crazy-Z7@users.noreply.github.com>
2023-11-24 21:57:55 +0800

    Delete ad/Keep.js

--- ad/Keep.js
+++
@@ -1,5 +0,0 @@
-let body=$response.body;
-body = body.replace(/tabType\":"entry"/g,'tabType":""');
-body = body.replace(/tabType\":"mall"/g,'tabType":""');
-body = body.replace(/type\":"homePrime"/g,'type":""');
-$done(body);
